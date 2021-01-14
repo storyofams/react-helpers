6f24b861ecc7b9bea70946b7e6d5282e05606829
Arvin Wilderink <arvinwilderink@me.com>
2021-01-14 12:02:13 +0100

    fix: global was not defined

--- src/hooks/useHasScreenWidth/useHasScreenWidth.test.tsx
+++ src/hooks/useHasScreenWidth/useHasScreenWidth.test.tsx
@@ -4,42 +4,42 @@ import { useHasScreenWidth } from './useHasScreenWidth';
 
 describe('[hooks] useHasScreenWidth', () => {
   it('should return false when screen width is wider than max width', () => {
-    globalThis.innerWidth = 1024;
+    global.innerWidth = 1024;
     const { result } = renderHook(() => useHasScreenWidth(0, 1023));
     expect(result.current).toBeFalsy();
   });
 
   it('should return true when screen width is narrower then max width', () => {
-    globalThis.innerWidth = 1024;
+    global.innerWidth = 1024;
     const { result } = renderHook(() => useHasScreenWidth(0, 1025));
     expect(result.current).toBeTruthy();
   });
 
   it('should return false when screen width is narrower than min width', () => {
-    globalThis.innerWidth = 212;
+    global.innerWidth = 212;
     const { result } = renderHook(() => useHasScreenWidth(213));
     expect(result.current).toBeFalsy();
   });
 
   it('should return true when screen width is wider than min width', () => {
-    globalThis.innerWidth = 212;
+    global.innerWidth = 212;
     const { result } = renderHook(() => useHasScreenWidth(211));
     expect(result.current).toBeTruthy();
   });
 
   it('should return true when screen width is within range', () => {
-    globalThis.innerWidth = 212;
+    global.innerWidth = 212;
     const { result } = renderHook(() => useHasScreenWidth(212, 212));
     expect(result.current).toBeTruthy();
   });
 
   it('should update when the window resizes', () => {
-    globalThis.innerWidth = 212;
+    global.innerWidth = 212;
     const { result } = renderHook(() => useHasScreenWidth(211));
     expect(result.current).toBeTruthy();
 
     act(() => {
-      globalThis.innerWidth = 210;
+      global.innerWidth = 210;
       fireEvent(window, new Event('resize'));
     });
 
